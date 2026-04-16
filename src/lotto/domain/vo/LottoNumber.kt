abf17d7000dc68ce452501da443c118344264ce9
joonamin <joonamin44@gmail.com>
2026-04-16 18:18:12 +0900

    feat(vo): payment, lotto context별 vo 추가

---
+++ src/lotto/domain/vo/LottoNumber.kt
@@ -0,0 +1,16 @@
+data class LottoNumber(
+    val number: Int,
+) {
+    init {
+        require(value in MIN_NUMBER..MAX_NUMBER) {
+            "$MIN_NUMBER ~ $MAX_NUMBER 사이의 로또 번호만 입력 가능합니다"
+        }
+    }
+
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+
+        fun random(): LottoNumber = LottoNumber((MIN_NUMBER..MAX_NUMBER).random())
+    }
+}
