58a9c3e7c2f63965857813f7efb64b1d3f45a958
joonamin <joonamin44@gmail.com>
2026-04-18 22:18:53 +0900

    refactor(money): lotto context, payment context의 money 정책의 성격이 다름에 따른 vo 분리
    - lotto context에서는 로또 티켓 구매 장수가 로또 금액 기준으로 선정됨
    - 사용자의 지갑내의 금액은 로또 정책과는 무관함

---
+++ src/lotto/domain/vo/TicketPrice.kt
@@ -0,0 +1,21 @@
+package lotto.domain.vo
+
+import Money
+
+data class TicketPrice(
+    val unitPrice: Money,
+) {
+    fun totalCost(ticketCount: Int): Money {
+        require(ticketCount > 0) {
+            "구매 수량은 1개 이상이어야 합니다"
+        }
+        return unitPrice * ticketCount
+    }
+
+    fun maxAffordableCount(budget: Money): Int = budget.value / unitPrice.value
+
+    companion object {
+        const val PRICE_PER_TICKET = 1000
+        val DEFAULT = TicketPrice(Money.won(PRICE_PER_TICKET))
+    }
+}
