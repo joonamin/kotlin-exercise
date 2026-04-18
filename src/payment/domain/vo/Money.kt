cad488d4c13dba0337e86c925d6bac008be059d6
joonamin <joonamin44@gmail.com>
2026-04-18 23:17:18 +0900

    fix(domain-invariant): payment context에서 충전 단위가 1000원 단위여야 한다는 도메인 규칙 추가
    - 추가적으로 ktlint 를 적용함

--- src/payment/domain/vo/Money.kt
+++ src/payment/domain/vo/Money.kt
@@ -5,12 +5,15 @@ data class Money(
         require(value >= 0) {
             "금액은 0 이상이여야 합니다"
         }
+        require(value % 1000 == 0) {
+            "금액은 1000원 단위여야 합니다"
+        }
     }
 
     operator fun plus(other: Money): Money = Money(this.value + other.value)
-    
+
     operator fun minus(other: Money): Money = Money(this.value - other.value)
-    
+
     operator fun times(multiplier: Int): Money = Money(this.value * multiplier)
 
     override fun compareTo(other: Money): Int = this.value.compareTo(other.value)
@@ -19,7 +22,7 @@ data class Money(
 
     companion object {
         val ZERO = Money(0)
-        
+
         fun won(value: Int) = Money(value)
     }
 }
