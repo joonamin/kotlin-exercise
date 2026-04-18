a131e3f4202a4eb5abd6322f4f6826575ca5ce06
joonamin <joonamin44@gmail.com>
2026-04-18 22:25:51 +0900

    feat(payment-context): 사용자 wallet 관련 aggregate 도출

---
+++ src/payment/domain/entity/Wallet.kt
@@ -0,0 +1,36 @@
+package payment.domain.entity
+
+import Money
+
+class Wallet(
+    private var _balance: Money = Money.ZERO,
+) {
+    val balance: Money get() = _balance
+
+    // command: 돈을 충전한다
+    fun charge(amount: Money) {
+        require(amount.value > 0) {
+            "충전 금액은 0보다 커야 합니다"
+        }
+        _balance += amount
+    }
+
+    // command: 출금한다 (로또 구매 등)
+    fun withdraw(amount: Money) {
+        require(amount.value > 0) {
+            "출금 금액은 0보다 커야 합니다"
+        }
+        require(_balance.isAffordable(amount)) {
+            "잔액이 부족합니다. (현재 잔액: ${_balance.value}원, 요청 금액: ${amount.value}원)"
+        }
+        _balance -= amount
+    }
+
+    // command: 당첨금을 입금한다 (lotto context -> 당첨금이 계산됨 이벤트에 의해 트리거)
+    fun receivePrize(amount: Money) {
+        require(amount.value > 0) {
+            "당첨금은 0보다 커야 합니다"
+        }
+        _balance += amount
+    }
+}
