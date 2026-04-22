781719375a3950a6b9c2d527ee1d294495429b39
joonamin <joonamin44@gmail.com>
2026-04-23 03:55:52 +0900

    refactor(lotto-round): 불필요한 backing property를 setter visibility 지정으로 수정
    - 기존 컬렉션은 가변 컬렉션을 인자로 받고, 불변 컬렉션을 get으로
      리턴하는 backing property의 선언은 불가피함
    - 레퍼런스가 바뀌지 않더라도 가변 리스트에 원소를 추가하는 방법으로
      사이드 이펙트를 발생시킬 가능성이 존재하기 때문

--- src/lotto/domain/entity/LottoRound.kt
+++ src/lotto/domain/entity/LottoRound.kt
@@ -10,35 +10,41 @@ import lotto.domain.vo.enums.RoundStatus
 class LottoRound(
     val roundNumber: Int,
     private val _pickedNumbers: MutableList<LottoNumbers>,
-    var status: RoundStatus = RoundStatus.OPEN,
+    status: RoundStatus = RoundStatus.OPEN,
 ) {
-    private var _winningNumbers: WinningNumbers? = null
-    private var _result: LottoResult? = null
+    var status: RoundStatus = status
+        private set
 
-    // getter 노출
+    var winningNumbers: WinningNumbers? = null
+        private set
+
+    var result: LottoResult? = null
+        private set
+
     val pickedNumbers: List<LottoNumbers> get() = _pickedNumbers.toList()
-    val result: LottoResult? get() = _result
-    val winningNumbers: WinningNumbers? get() = _winningNumbers
 
     // repository 에서 상태를 복원하기 위해서 열어둔 백도어
     fun restoreState(
         winningNumbers: WinningNumbers?,
         result: LottoResult?,
     ) {
-        this._winningNumbers = winningNumbers
-        this._result = result
+        this.winningNumbers = winningNumbers
+        this.result = result
     }
 
     // command: 이번 회차의 로또를 추첨한다.
     fun draw(winningNumbers: WinningNumbers): LottoResult {
-        require(status == RoundStatus.OPEN) {
+        // REVIEW: require은 기존 생성자로 전달되는 값들의 검증에 사용
+        // check는 객체 내부의 상태에 따른 불변성 검증에 사용 (~~실행 전 ~상태를 보장 등등...)
+        // exception 타입이 다르더라 -> require은 IllegalArgumentException, check는 IllegalStateException
+        check(status == RoundStatus.OPEN) {
             "${roundNumber}회차는 이미 추첨이 완료되었습니다."
         }
-        require(_pickedNumbers.isNotEmpty()) {
+        check(_pickedNumbers.isNotEmpty()) {
             "이번 회차에 구매한 로또가 없습니다"
         }
 
-        this._winningNumbers = winningNumbers
+        this.winningNumbers = winningNumbers
         this.status = RoundStatus.FINISHED
 
         // 구매한 로또 티켓들의 aggregation 도출
@@ -50,7 +56,7 @@ class LottoRound(
             }
 
         val drawResult = LottoResult(results)
-        this._result = drawResult
+        this.result = drawResult
 
         // 아래는 만약 이벤트 발행 인프라가 있다면 추가 수행
         // 현재는 바로 결과값을 리턴하여 콘솔에 보여주기 위함
@@ -58,10 +64,10 @@ class LottoRound(
     }
 
     // domain service
-    fun summary(): String = "$roundNumber,${this.status.name},${_winningNumbers},${_pickedNumbers}"
+    fun summary(): String = "$roundNumber,${this.status.name},$winningNumbers,${_pickedNumbers}"
 
     fun addTicket(numbers: LottoNumbers) {
-        require(status == RoundStatus.OPEN) {
+        check(status == RoundStatus.OPEN) {
             "이미 마감된 회차에는 티켓을 추가할 수 없습니다"
         }
         _pickedNumbers.add(numbers)
